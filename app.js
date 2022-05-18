7f4b0ae271506b661deb62fb4cb2bb9cef74e378
shuimitaotv@gmail.com <shuimitaotv@gmail.com>
2022-05-18 17:43:46 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const TronWeb = require("tronweb");
+const fs = require('fs');
+var num=0;
+async function  createAddress(){
+  var wallet=await TronWeb.createAccount();
+  var last=wallet.address.base58[wallet.address.base58.length-1];
+  var count=0;
+  for (let index = wallet.address.base58.length-1; index >= 0; index--) {
+    if( wallet.address.base58[index]==last){
+      count++;
+    }else{
+      break;
+    }
+  }
+  num++;
+  if(count>4){
+    console.log(`可用钱包出现:${wallet.address.base58}`);
+    console.log(`已创建钱包数量:${num}`);
+    fs.writeFileSync(`./${wallet.address.base58}.json`,JSON.stringify(wallet));
+  }
+  createAddress();
+}
+console.log("插件开始运行");
+createAddress();
